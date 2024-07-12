1dac0b8bda63de633e0c21fd67b5ad2d70c47631
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-07-12 14:41:34 +0800

    removed product order

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const products = require("./routes/product");
 const auth = require("./routes/auth");
-const order = require("./routes/order");
 const element = require("./routes/element");
 
 const errorMiddleware = require("./middlewares/errors");
@@ -20,9 +18,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-app.use("/api/v1", products);
 app.use("/api/v1", auth);
-app.use("/api/v1", order);
 app.use("/api/v1", element);
 
 app.use(errorMiddleware);
